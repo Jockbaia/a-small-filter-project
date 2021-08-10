b941c8045a5078984f204caf021446d39d706c14
Aurora Anna Pia Sergio <s290363@studenti.polito.it>
2021-08-10 21:50:45 +0200

    prova filtro labbra (finito)

--- main.py
+++ main.py
@@ -3,40 +3,43 @@ import dlib
 import keyboard
 import numpy as np
 
+
 def change_lips(img, lm):
-    upper_lip = [lm.part(48).x, lm.part(48).y,
-                 lm.part(49).x, lm.part(49).y,
-                 lm.part(50).x, lm.part(50).y,
-                 lm.part(51).x, lm.part(51).y,
-                 lm.part(52).x, lm.part(52).y,
-                 lm.part(53).x, lm.part(53).y,
-                 lm.part(54).x, lm.part(54).y,
-                 lm.part(64).x, lm.part(64).y,
-                 lm.part(63).x, lm.part(63).y,
-                 lm.part(62).x, lm.part(62).y,
-                 lm.part(61).x, lm.part(61).y,
-                 lm.part(60).x, lm.part(60).y]
-    lower_lip = [lm.part(45).x, lm.part(45).y,
-                 lm.part(59).x, lm.part(59).y,
-                 lm.part(58).x, lm.part(58).y,
-                 lm.part(57).x, lm.part(57).y,
-                 lm.part(56).x, lm.part(56).y,
-                 lm.part(55).x, lm.part(55).y,
-                 lm.part(54).x, lm.part(54).y,
-                 lm.part(64).x, lm.part(64).y,
-                 lm.part(65).x, lm.part(65).y,
-                 lm.part(66).x, lm.part(66).y,
-                 lm.part(67).x, lm.part(67).y,
-                 lm.part(60).x, lm.part(60).y]
-    mask = np.zeros(img.shape)
-    cv2.fillPoly(mask, np.array([upper_lip]), (0, 0, 255))
-    cv2.fillPoly(mask, np.array([lower_lip]), (0, 0, 255))
-    return cv2.addWeighted(img, 1, mask, 0.5, 0.0, img)
+    upper_lip = [(lm.part(48).x, lm.part(48).y),
+                 (lm.part(49).x, lm.part(49).y),
+                 (lm.part(50).x, lm.part(50).y),
+                 (lm.part(51).x, lm.part(51).y),
+                 (lm.part(52).x, lm.part(52).y),
+                 (lm.part(53).x, lm.part(53).y),
+                 (lm.part(54).x, lm.part(54).y),
+                 (lm.part(64).x, lm.part(64).y),
+                 (lm.part(63).x, lm.part(63).y),
+                 (lm.part(62).x, lm.part(62).y),
+                 (lm.part(61).x, lm.part(61).y),
+                 (lm.part(60).x, lm.part(60).y)]
+    lower_lip = [(lm.part(48).x, lm.part(48).y),
+                 (lm.part(59).x, lm.part(59).y),
+                 (lm.part(58).x, lm.part(58).y),
+                 (lm.part(57).x, lm.part(57).y),
+                 (lm.part(56).x, lm.part(56).y),
+                 (lm.part(55).x, lm.part(55).y),
+                 (lm.part(54).x, lm.part(54).y),
+                 (lm.part(64).x, lm.part(64).y),
+                 (lm.part(65).x, lm.part(65).y),
+                 (lm.part(66).x, lm.part(66).y),
+                 (lm.part(67).x, lm.part(67).y),
+                 (lm.part(60).x, lm.part(60).y)]
+    mask = np.zeros(img.shape, dtype=np.uint8)
+    cv2.fillPoly(mask, np.array([upper_lip]), (0, 0, 80))
+    cv2.fillPoly(mask, np.array([lower_lip]), (0, 0, 80))
+    return cv2.addWeighted(img, 1, mask, 0.2, 0.0, img)
 
 
 def apply_mask(flag, image, landmarks):   #flag per identificare la parte del viso da modificare (es. 1 - naso, 2 - bocca, ecc...)
-    if flag == 2: #esempio stupido per cambiare colore labbra
+    if flag == 2:
         res = change_lips(image, landmarks)
+    else:
+        res = image
     return res
 
 
@@ -99,10 +102,10 @@ def main():
                                         0.2, (211, 211, 211), 1, cv2.LINE_AA, False)
 
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            #final_frame = apply_mask(2, frame, landmarks)
+            final_frame = apply_mask(2, frame, landmarks)
 
-        cv2.imshow("main", frame)
-        #cv2.imshow("main", final_frame)
+        #cv2.imshow("main", frame)
+        cv2.imshow("main", final_frame)
         cv2.waitKey(1)
 
 
